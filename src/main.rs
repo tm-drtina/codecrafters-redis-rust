923c330866ef40f41f7fe23cd941de7261b9f278
Tomas Drtina <tm.drtina@gmail.com>
2024-03-01 19:49:55 +0100

    Add arg for listening on non-default port

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,45 @@
+use std::net::{IpAddr, SocketAddr};
+
+use anyhow::{bail, Context};
 use tokio::net::TcpListener;
 
 use redis_starter_rust::{Connection, Server};
 
+struct Config {
+    host: IpAddr,
+    port: u16,
+}
+
+fn parse_args() -> anyhow::Result<Config> {
+    let mut args = std::env::args();
+    args.next()
+        .context("Expected first arg (path of executable)")?;
+
+    let mut config = Config {
+        host: "127.0.0.1".parse().unwrap(),
+        port: 6379
+    };
+
+    while let Some(arg) = args.next() {
+        match arg.as_str() {
+            "--port" => {
+                config.port = args
+                    .next()
+                    .context("Argument port is missing a value")?
+                    .parse()
+                    .context("Invalid value for port arg")?
+            }
+            _ => bail!("Unrecognized argument {arg}")
+        }
+    }
+    Ok(config)
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:6379").await?;
+    let config = parse_args()?;
+    let addr = SocketAddr::new(config.host, config.port);
+    let listener = TcpListener::bind(addr).await?;
     let server = Server::new();
 
     loop {
